85938d9189457758a00c53af6d7066292c0be2ae
drocodes <alejandro.saenz@live.com>
2020-06-17 12:52:05 -0400

    fixing routes

--- routes/chuck.js
+++ routes/chuck.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/chuck", async (req, res) => {
   const response = await axios.get("https://api.chucknorris.io/jokes/random");
   console.log(response);
   res.send(response.data.value);
--- routes/ping.js
+++ routes/ping.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/ping", (req, res) => {
   res.send("pong");
 });
 
